c94ea8131f3e9f6206569247f4733d1a091d520e
abrichards <angelarichards261@gmail.com>
2024-01-06 12:12:11 -0800

    update Name creation in account

--- lib/account.dart
+++ lib/account.dart
@@ -36,161 +36,89 @@ class AccountState extends State<Account> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
-
-    Widget gradientDecor() {
-      return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 253, 207, 255),
-              Color.fromARGB(255, 255, 255, 255),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.center,
-          ),
+  Widget gradientDecor() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 253, 207, 255),
+            Color.fromARGB(255, 255, 255, 255),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.center,
         ),
-      );
-    }
+      ),
+    );
+  }
 
-    return Stack(
-      children: <Widget>[
-        gradientDecor(),
-        Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+  CircleAvatar userIcon(height) {
+    return CircleAvatar(
+      backgroundImage: const AssetImage(
+          'assets/elmo_on_fire.png'), // TODO: allow user to get image
+      radius: height / 10,
+    );
+  }
+
+  Form userName() {
+    return Form(
+      key: formKey1,
+      child: Column(
+        children: [
+          GestureDetector(
+            child: SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Text(
+                _textEditingController1.text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: mainFont.fontFamily,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
             ),
-            backgroundColor: mainColor,
-          ),
-          backgroundColor: Colors.transparent,
-          body: Stack(
-            children: <Widget>[
-              confetti(_controllerCenter),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: EdgeInsets.only(top: height / 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage:
-                            const AssetImage('assets/elmo_on_fire.png'),
-                        radius: height / 10,
-                      ),
-                      SizedBox(height: height / 30),
-                      Form(
-                        key: formKey1,
-                        child: Column(
-                          children: [
-                            GestureDetector(
-                              child: Text(
-                                _textEditingController1.text,
-                                style: TextStyle(
-                                    fontFamily: mainFont.fontFamily,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                              ),
-                              onTap: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return Card(
-                                      margin: EdgeInsets.fromLTRB(
-                                          50,
-                                          MediaQuery.of(context).size.height /
-                                              2.5,
-                                          50,
-                                          MediaQuery.of(context).size.height /
-                                              2.5),
-                                      child: TextField(
-                                        controller: _textEditingController1,
-                                        style: mainFont,
-                                        decoration: InputDecoration(
-                                          hintText: "Name",
-                                          labelText: '',
-                                        ),
-                                        autofocus: false,
-                                        maxLength: 30,
-                                        maxLengthEnforcement:
-                                            MaxLengthEnforcement.enforced,
-                                        onSubmitted: (value) {
-                                          PrefsHelper().accountName = value;
-                                          _textEditingController1.text = value;
-                                          _controllerCenter.play(); // Confetti!
-                                          Navigator.pop(context);
-                                          setState(() {});
-                                        },
-                                      ),
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                          ],
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return Card(
+                    margin: EdgeInsets.fromLTRB(
+                      70,
+                      MediaQuery.of(context).size.height / 2.5,
+                      70,
+                      MediaQuery.of(context).size.height / 2.5,
+                    ),
+                    child: SizedBox(
+                      width: 250,
+                      child: TextField(
+                        controller: _textEditingController1,
+                        style: mainFont,
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Name",
+                          labelText: 'Who are you?',
+                          contentPadding: EdgeInsets.all(20),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: height / 2.2),
-                child: Container(
-                  color: Colors.white,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: height / 2.6, left: width / 20, right: width / 20),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        color: Color.fromARGB(255, 254, 235, 255),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black45,
-                              blurRadius: 2.0,
-                              offset: Offset(0.0, 2.0))
-                        ],
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(width / 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            headerChild('Level', "114"),
-                            // headerChild('Skills', "1205"),
-                            headerChild('ⴵ Time Spent', timeSpent()),
-                          ],
-                        ),
+                        maxLength: 30,
+                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                        onSubmitted: (value) {
+                          PrefsHelper().accountName = value;
+                          _textEditingController1.text = value;
+                          _controllerCenter.play(); // Confetti!
+                          Navigator.pop(context);
+                          setState(() {});
+                        },
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: height / 20),
-                      child: Column(
-                        children: <Widget>[
-                          infoChild(width / 3, Icons.email, 'email'),
-                          infoChild(width / 3, Icons.call, 'phone number'),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                  );
+                },
+              );
+            },
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -292,57 +220,154 @@ class AccountState extends State<Account> {
     return time.toString() + label;
   }
 
-  Widget headerChild(String header, String value) => Expanded(
-        child: Column(
+  Widget headerChild(String header, String value) {
+    return Expanded(
+      child: Column(
+        children: <Widget>[
+          Text(
+            header,
+            style: GoogleFonts.comicNeue(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            '$value',
+            style: GoogleFonts.comicNeue(
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget infoChild(double width, IconData icon, data) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: InkWell(
+        child: Row(
           children: <Widget>[
+            SizedBox(
+              width: width / 10,
+            ),
+            Icon(
+              icon,
+              color: Color.fromARGB(255, 232, 136, 235),
+              size: 36.0,
+            ),
+            SizedBox(
+              width: width / 20,
+            ),
             Text(
-              header,
+              data,
               style: GoogleFonts.comicNeue(
                 fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Text(
-              '$value',
-              style: GoogleFonts.comicNeue(
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
               ),
             )
           ],
         ),
-      );
+        onTap: () {
+          print('Info Object selected');
+        },
+      ),
+    );
+  }
 
-  Widget infoChild(double width, IconData icon, data) => Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: InkWell(
-          child: Row(
-            children: <Widget>[
-              SizedBox(
-                width: width / 10,
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Stack(
+      children: [
+        gradientDecor(),
+        Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            backgroundColor: mainColor,
+          ),
+          backgroundColor: Colors.transparent,
+          body: ListView(
+            children: [
+              Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: height / 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          userIcon(height),
+                          SizedBox(height: height / 30),
+                          userName(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: height / 2.2),
+                    child: Container(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: height / 2.6, left: width / 20, right: width / 20),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: Color.fromARGB(255, 254, 235, 255),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black45,
+                                  blurRadius: 2.0,
+                                  offset: Offset(0.0, 2.0))
+                            ],
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(width / 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                headerChild('Level', "114"),
+                                // headerChild('Skills', "1205"),
+                                headerChild('ⴵ Time Spent', timeSpent()),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: height / 20),
+                          child: Column(
+                            children: <Widget>[
+                              infoChild(width / 3, Icons.email, 'email'),
+                              infoChild(width / 3, Icons.call, 'phone number'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  confetti(_controllerCenter),
+                ],
               ),
-              Icon(
-                icon,
-                color: Color.fromARGB(255, 232, 136, 235),
-                size: 36.0,
-              ),
-              SizedBox(
-                width: width / 20,
-              ),
-              Text(
-                data,
-                style: GoogleFonts.comicNeue(
-                  fontSize: 18.0,
-                ),
-              )
             ],
           ),
-          onTap: () {
-            print('Info Object selected');
-          },
         ),
-      );
+      ],
+    );
+  }
 }
